1b2a55f08033811abacd4871c29ca3efbc5b4363
Marco <marco.berbeglia@gmail.com>
2019-10-03 14:16:19 +0200

    stable v1.2

---
+++ giocopalla_project/Assets/Scripts/GameScene.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameScene : MonoBehaviour
+{
+    public float GameTime { get; set; }         // maximum time to catch the new circle
+
+    public int CurrentScore { get; set; }           // score of the current game
+
+    public int HighScore { get; set; }          // high score of the player
+
+    public GameObject LosingPanel;          // panel shown when losing and "CurrentScore" is less than "HighScore"
+    public Text LosingPanelCurrentScoreText;            // text of "CurrentScore" at losing time
+    public Text LosingPanelHighScoreText;           // text of "HighScore" of the player
+
+    public GameObject WinningPanel;         // panel shown when losing and "CurrentScore" is higher than "HighScore"
+    public Text WinningPanelNewHighScoreText;           // text of new "HighScore" of the player
+
+    public GameObject BackToMenu;           // button that brings back to menu
+
+    public Text DisplayedScoreText;         // text of "CurrentScore" in scene when playing
+
+    private GoogleAdMobBanners GoogleAdMobBanners;          // object that manages banners
+
+    public void Start()
+    {
+        this.GoogleAdMobBanners = this.gameObject.GetComponent<GoogleAdMobBanners>();
+
+        // objects that need to be deactivated at start
+        this.LosingPanel.gameObject.SetActive(false);
+        this.WinningPanel.gameObject.SetActive(false);
+        this.BackToMenu.gameObject.SetActive(false);
+
+        // sets "GameTime" using "EstimateTimer" function
+        this.GameTime = this.EstimateTimer();
+        Debug.Log("gameTime: " + this.GameTime);
+
+        // sets "CurrentScore" to zero
+        this.CurrentScore = 0;
+
+        // retrieves "HighScore" from player preferences or zero
+        this.HighScore = PlayerPrefs.GetInt("high_score", 0);
+    }
+
+    public void Update()
+    {
+        // updates "DisplayedScoreText" with "CurrentScore"
+        this.DisplayedScoreText.text = this.CurrentScore.ToString();
+    }
+
+    /* float EstimateTimer(): determines game time based on dimensions of the screen */
+    private float EstimateTimer()   // TROVA UN ALGORITMO CHE CALCOLI IL TEMPO GIUSTO SU OGNI SCHERMO
+    {
+        float screenWidth;
+        float screenHeight;
+        float gameTime;
+
+        Debug.Log("width: " + Screen.width + " height: " + Screen.height + " dpi: " + Screen.dpi);
+
+        if (Screen.dpi != 0)
+        {
+            screenWidth = Screen.width / Screen.dpi;
+            screenHeight = Screen.height / Screen.dpi;
+            gameTime = (Mathf.Sqrt(Mathf.Pow(screenWidth, 2) + Mathf.Pow(screenHeight, 2))) / 10f;
+        }
+        else
+        {
+            gameTime = 0.52f;
+        }
+
+        return gameTime;
+    }
+
+    /* void CheckHighScore(): loads and shows banners, then checks if player has made new high score and shows related panel */
+    public void CheckHighScore()
+    {
+        this.GoogleAdMobBanners.LoadBanners();
+        this.GoogleAdMobBanners.ShowBanners();
+
+        if ( this.CurrentScore > this.HighScore )
+        {
+            PlayerPrefs.SetInt("high_score", this.CurrentScore);
+            this.HighScore = PlayerPrefs.GetInt("high_score");
+            ShowNewHighScoreBox();
+        }
+        else
+        {
+            ShowLosingBox();
+        }
+    }
+
+    /* void ShowNewHighScore(): shows "WinningPanel" and related */
+    private void ShowNewHighScoreBox()
+    {
+        WinningPanelNewHighScoreText.text = this.HighScore.ToString();
+
+        WinningPanel.gameObject.SetActive(true);
+        BackToMenu.gameObject.SetActive(true);
+
+        // to avoid ugly graphical effects
+        this.DisplayedScoreText.gameObject.SetActive(false);
+    }
+
+    /* void ShowLosingBox(): show "LosingPanel" and related */
+    private void ShowLosingBox()
+    { 
+        LosingPanelCurrentScoreText.text = this.CurrentScore.ToString();
+        LosingPanelHighScoreText.text = "HIGH SCORE: " + this.HighScore.ToString();
+
+        LosingPanel.gameObject.SetActive(true);
+        BackToMenu.gameObject.SetActive(true);
+
+        // to avoid ugly graphical effect
+        this.DisplayedScoreText.gameObject.SetActive(false);
+    }
+
+    /* void TryAgain(): sets scene for new game */
+    public void TryAgain()
+    {
+        LosingPanel.gameObject.SetActive(false);
+        WinningPanel.gameObject.SetActive(false);
+        BackToMenu.gameObject.SetActive(false);
+
+        this.DisplayedScoreText.gameObject.SetActive(true);
+
+        this.GoogleAdMobBanners.HideBanners();
+
+        this.CurrentScore = 0;
+    }
+
+    /* void GoBacktoMenu(): loads menu scene */
+    public void GoBackToMenu()
+    {
+        this.GoogleAdMobBanners.HideBanners();
+
+        SceneManager.LoadScene("Menu");
+    }
+}
---
+++ giocopalla_project/Assets/Scripts/PointingArrow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PointingArrow : MonoBehaviour
+{
+    void Start()
+    {
+        //for_testing PlayerPrefs.SetString("first_time", "true");
+
+        //from first run of app until user open "InfoPanel", pref will be !=false
+        if ( PlayerPrefs.GetString("first_time").Equals("false") )
+            this.gameObject.SetActive(false);
+        else
+        {
+            this.gameObject.SetActive(true);
+            StartCoroutine(ArrowBouncing());
+        }
+    }
+
+    void Update()
+    {
+        if (PlayerPrefs.GetString("first_time").Equals("false"))
+            this.gameObject.SetActive(false);
+    }
+
+    /* IEnumerator ArrowBouncing(): bounces the arrow until "first_time" pref is false */
+    private IEnumerator ArrowBouncing()
+    {
+        while (!PlayerPrefs.GetString("first_time").Equals("false"))
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                this.gameObject.transform.Translate(Vector2.down * 0.5f);
+                yield return new WaitForEndOfFrame();
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                this.gameObject.transform.Translate(Vector2.up * 0.5f);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+}
